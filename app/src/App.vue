66f6e6897c727d4d0f1e601e976d49058d99befa
Joshua Handschin <josh.handschin@usu.edu>
2020-03-10 15:44:09 -0600

    restructured the app location

---
+++ app/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <h1>Auction House App</h1>
+    <div class="nav">
+      <!-- routes you created in src/router/index.js -->
+      <router-link class="rLink button" to='/home'>Home</router-link>
+      <router-link class="rLink button" to='/register'>Register</router-link>
+      <router-link class="rLink button alert" to='/login'>Login</router-link>
+    </div>
+    <!-- components can be part of a greater component -->
+    <!-- import component, have a components obj with component name, put in html as it's on tag -->
+    <InsideComponent value="I'm a value passed as a prop" />
+    <!-- value="I'm..." is a prop. Basically an object that can be used in the child -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+// All other components are from routerlink but if you want to
+// nest compoenets you can import them then just used them inside
+import InsideComponent from './components/InsideComponent.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+  components: {
+    InsideComponent,
+  }
+}
+</script>
+
+<style>
+.nav {
+  margin-bottom: 1rem;
+}
+.rLink.button {
+  padding: 1rem;
+  margin-right: 1rem;
+  border-style: solid; 
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/Home.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="hello">
+    <h1>{{ greeting }}</h1>
+    
+    <!-- v-for loops through the data received -->
+    <div v-for="item in itemList" :key="item.id">
+      <div>Title: {{item.title}} Description: {{item.description}}</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import db from '../firebase';
+require('firebase/firestore');
+export default {
+  name: 'Home',
+  data() {
+    return {
+      greeting: 'Buy All The Things!!!',
+      list: [],
+      userName: 'Jawsome Possum',
+      first: 'Josh',
+      last: 'Handschin',
+    }
+  },
+
+  created() {
+    // Getting data from 'Item' Collection
+    db.collection('Item').get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.data());
+        const data = {
+          id: doc.id,
+          title: doc.data().title,
+          description: doc.data().description,
+        }
+        this.list.push(data);
+      })
+    })
+    .catch(error => {
+      console.log(error.data);
+    });
+
+    // Adding to 'Users" Collection
+    db.collection('User').doc(this.userName).set({
+      user_name: this.userName,
+      name: this.first + ' ' + this.last,
+    })
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(error) {
+      console.log('error' , error);
+    })
+  },
+
+  computed: {
+    itemList() {
+    return this.list;
+    }
+  },
+
+  methods: {
+  },
+};
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
