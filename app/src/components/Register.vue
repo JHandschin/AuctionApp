71aa9fa5d9cd7e25fef8d036eb9cbc0f0bd500d2
Joshua Handschin <josh.handschin@usu.edu>
2020-03-11 15:07:34 -0600

    Set up Register.vue

--- app/src/components/Register.vue
+++ app/src/components/Register.vue
@@ -1,11 +1,85 @@
 <template>
-    <h1>Register</h1>    
+    <div class="content">
+        <h1>Register for a New Account</h1>    
+        <div>Please enter a User Name</div>
+        <input v-model="userName" type="text" name="userName" id="userName" placeholder="Unique User Name" >
+        <div>Please enter your First Name</div>
+        <input v-model="firstName" type="text" name="firstName" id="firstName" placeholder="First Name" >
+        <div>Please enter your Last Name</div>
+        <input v-model="lastName" type="text" name="lastName" id="lastName" placeholder="Last Name" >
+        <div>Please enter a User Name</div>
+        <input v-model="email" type="text" name="email" id="email" placeholder="Email" >
+        <button @click="submit" class="button">Submit</button>
+    </div>
 </template>
 <script>
+
+import db from '../firebase';
+require('firebase/firestore');
+
 export default {
     name: 'Register',
+    data() {
+        return {
+            users: [],
+            userName: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+        }
+    },
+    methods: {
+        submit() {
+            let failed = false;
+            this.users = [];
+            db.collection('User').get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        // console.log(doc.data());
+                        this.users.push(doc.data().user_name);
+                    })
+                })
+                .then(() => {
+                    if (this.users.includes(this.userName)) {
+                        console.log('Sorry, that user-name is already taken');
+                    } else {
+                        console.log('Gratz!!');
+                        
+                        const payload = {
+                            userName: this.userName,
+                            name: this.firstName + " " + this.lastName,
+                            email: this.email,
+                        }
+                        // Adding to 'User" Collection
+                        db.collection('User').doc(this.userName).set(payload)
+                            .then(function() {
+                            console.log('success');
+                        })
+                            .catch((error) => {
+                            console.log('error' , error);
+                            failed = true;
+                        })
+                    }
+                    if (failed) {
+                        this.cleansePage();
+                    }
+                })
+                .catch(error => {
+                console.log(error);
+            });
+        },
+        cleansePage() {
+            this.userName = '';
+            this.firstName = '';
+            this.lastName = '';
+            this.email = '';
+        }
+    },
 }
 </script>
-<style lang="sass" scoped>
+<style scoped>
+    h1 {
+        margin-top: 2rem;
+    }
 
 </style>
---
+++ app/src/components/firebaseRoutes.vue
@@ -0,0 +1,85 @@
+<template>
+    
+</template>
+
+<script>
+
+import db from '../firebase';
+require('firebase/firestore');
+
+export default {
+    data() {
+        return {
+            Auction: {
+                id: '',
+                users: [],
+                items: [],
+                date: '',
+                admin: '',
+            },
+            Bid: {
+                id: '',
+                userId: '',
+                ItemId: '',
+                price: 0.00,
+                date: '',
+            },
+            User: {
+                id: '',
+                isAdmin: false,
+                isPresenter: false,
+                name: '',
+                phone: '',
+                email: '',
+                userName: '',
+                userPass: '',
+                bids: [],
+            },
+            Item: {
+                id: '',
+                auctionId: '',
+                title: '',
+                description: '',
+                media: [],
+                bids: [],
+                price: 0.00,
+                isLive: false,
+                isApproved: false,
+            },
+            Media: {
+                id: '',
+                itemId: '',
+            }
+        }
+    },
+    methods: {
+        getAuctions() {
+            db.collection('Auction').get()
+                .then(querySnapshot => {
+                    querySnapshot.forEach(doc => {
+                        console.log(doc.data());
+                        this.list.push(this.Auction);
+                    })
+                })
+                .catch(error => {
+                console.log(error.data);
+            });
+        },
+        setAuction() {
+            // Adding to 'Users" Collection
+            db.collection('Auction').doc(this.Auction.date).set(this.Auction)
+                .then(function() {
+                console.log('success');
+            })
+                .catch((error) => {
+                console.log('error' , error);
+            })
+        },
+
+    },
+
+}
+</script>
+<style scoped>
+
+</style>
