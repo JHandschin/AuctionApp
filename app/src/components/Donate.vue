92832dd4a0529c16c85e916516b77c01edf08ddf
ryan-egbert <ryanegbert@aggiemail.usu.edu>
2020-04-14 13:20:51 -0600

    Updated strings to booleans

--- app/src/components/Donate.vue
+++ app/src/components/Donate.vue
@@ -106,7 +106,7 @@ export default {
             price: '',
             title: '',
             description: '',
-            isSilent: '',
+            isSilent: true,
         }
     },
     methods: {
@@ -120,7 +120,7 @@ export default {
           title: this.title,
           description: this.description,
           isSilent: this.isSilent,
-          isVerified: 'false',
+          isVerified: false,
         }
         db.collection('Item').doc(this.title).set(payload).then(function() {
             //console.log('success');
